4ea21a8524b7eab1c4c82f662505153322c16030
ThibaudAv <thibaud.avenier@gmail.com>
2024-06-12 10:50:56 +0200

    chore: clean code & bump mvn dependancy version

--- src/main/java/io/gravitee/policy/template/TemplatePolicy.java
+++ src/main/java/io/gravitee/policy/template/TemplatePolicy.java
@@ -15,22 +15,6 @@
  */
 package io.gravitee.policy.template;
 
-/**
- * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import io.gravitee.common.http.HttpStatusCode;
 import io.gravitee.gateway.reactive.api.ExecutionFailure;
 import io.gravitee.gateway.reactive.api.context.HttpExecutionContext;
@@ -40,10 +24,6 @@ import io.gravitee.policy.template.v3.TemplatePolicyV3;
 import io.reactivex.rxjava3.core.Completable;
 import lombok.extern.slf4j.Slf4j;
 
-/**
- * @author Yann TAVERNIER (yann.tavernier at graviteesource.com)
- * @author GraviteeSource Team
- */
 @Slf4j
 public class TemplatePolicy extends TemplatePolicyV3 implements Policy {
 
